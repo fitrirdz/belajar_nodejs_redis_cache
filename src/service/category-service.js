9f8435f2d163b2ac273c7e721c58b8bed1171168
khannedy <echo.khannedy@gmail.com>
2025-06-27 11:23:52 +0700

    Add category API endpoint with supporting controller, service, and database schema changes

---
+++ src/service/category-service.js
@@ -0,0 +1,33 @@
+import {prismaClient} from "../application/database.js";
+
+const findAll = async () => {
+  // query semua parent
+  const parents = await prismaClient.category.findMany({
+    where: {
+      parent_id: null
+    },
+    select: {
+      id: true,
+      name: true
+    }
+  })
+
+  // iterasi semua parent, tambahkan children
+  for (let parent of parents) {
+    parent.children = await prismaClient.category.findMany({
+      where: {
+        parent_id: parent.id
+      },
+      select: {
+        id: true,
+        name: true
+      }
+    })
+  }
+
+  return parents;
+}
+
+export default {
+  findAll
+}
