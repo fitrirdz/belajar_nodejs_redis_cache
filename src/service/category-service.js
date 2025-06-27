f39c78e8956568e34b481865e8cdbc69d42a55fb
khannedy <echo.khannedy@gmail.com>
2025-06-27 12:33:34 +0700

    Implement caching for category service using Redis and update dependencies

--- src/service/category-service.js
+++ src/service/category-service.js
@@ -1,6 +1,21 @@
 import {prismaClient} from "../application/database.js";
+import Redis from "ioredis";
+
+const redis = new Redis({
+  host: "localhost",
+  port: 6379,
+  db: 0,
+});
 
 const findAll = async () => {
+  // apakah ada di redis atau tidak?
+  const json = await redis.get("categories");
+
+  // kalo ada, kita kembalikan langsung yang di redis
+  if(json) return JSON.parse(json);
+
+
+  // kalo gak ada, kita query ke database, lalu simpan di redis
   // query semua parent
   const parents = await prismaClient.category.findMany({
     where: {
@@ -12,6 +27,9 @@ const findAll = async () => {
       children: true
     }
   })
+
+  // simpan ke redis
+  await redis.setex("categories", 60 * 60, JSON.stringify(parents));
 
   // iterasi semua parent, tambahkan children
   // for (let parent of parents) {
